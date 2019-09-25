108956b72f1f8588d084112cacac26818ee4e993
renatojobal <renatojobal@gmail.com>
2019-09-25 17:15:25 -0500

    Creando modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,63 @@ import React from "react";
 
 class Modal extends React.Component {
   render() {
-    return <div>This is the Modal page</div>;
+    return (
+      <div
+        class="modal fade"
+        id="exampleModalCenter"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalCenterTitle">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="container">
+                <div class="row">
+                  <div class="col-3">
+                    Primera fila primera columna
+                  </div>
+                  <div class="col-9">
+                    Primera fila segunda columna
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    Segunda fila segeunda columna
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
