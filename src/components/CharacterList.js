7e5655db02ca2294d5b5176aff0a1fb649ba0d4d
renatojobal <renatojobal@gmail.com>
2019-09-30 18:11:31 -0500

    Creando la rama de charater page con la estrucutra (aun no funciona)

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,38 +1,31 @@
 import React from "react";
-import CharacterContainer from './CharacterContainer'
-import styled from 'styled-components'
-import PropTypes from 'prop-types';
-
-const Container = styled.div`
-  display: grid;
-  align-items: center;
-  grid-gap: 20px;
-  grid-template-columns: 200px 200px 200px 200px 200px;
-`
+import CharacterContainer from "./CharacterContainer";
+import PropTypes from "prop-types";
 
 class CharacterList extends React.Component {
   render() {
-
-    const { images = [] } = this.props;
-
-    return (
-      <Container>
-        {images.map(image => {
-          return (
-            <CharacterContainer
-              imageURL={image}
-              onClick={() => console.log(image)}
-            />
-          )
-        })}
-      </Container>
-    );
+    if (this.props.characters) {
+      // RENDERIZAR LAS IMAGENES
+      return (
+        <div className="container">
+          {this.props.characters.map((character) => {
+            return <CharacterContainer imageURL={character.imageURL} />;
+          })}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          404. Actualmente no hay nada en comicList, es decir no esta llamando a
+          la api
+        </div>
+      );
+    }
   }
 }
 
 CharacterList.propTypes = {
-  images : PropTypes.array,
-  image: PropTypes.string,
+  characters: PropTypes.array
 };
 
 export default CharacterList;
