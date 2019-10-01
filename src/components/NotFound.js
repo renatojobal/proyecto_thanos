a38fe3b81ea6eb4c221516875b124dbf4fa658e9
chjuca <chjuca@utpl.edu.ec>
2019-10-01 13:52:22 -0500

    Agregando Pagina 404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  position:fixed;
+  width:500px;
+  height:400px;
+  left:50%;
+  top:50%;
+  margin-left:-150px;
+  margin-top:-100px;
+`;
+
+const Image = styled.img`
+  width:500px;
+  height:600px;
+`;
 
 class NotFound extends React.Component {
   render() {
-    return <h1>Not Found</h1>;
+    return (
+      <Container>
+        <Image
+          src="https://tenor.com/view/thanos-avengers-infinity-war-sorry-gif-12454554.gif"
+          alt="perdoname niñita"
+        ></Image>
+        <span>No Encontrado...</span>
+      </Container>
+    );
   }
 }
 
